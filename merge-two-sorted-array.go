a93cf204249321c0b07fe5736a9491ec13423fcf
Ricardo Nascimento <43101705+ricardonas@users.noreply.github.com>
2024-12-26 17:13:56 -0300

    Update merge-two-sorted-array.go

--- merge-two-sorted-array.go
+++ merge-two-sorted-array.go
@@ -1,22 +1,22 @@
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	pivot := (m + n) - 1
+	writeIndex := (m + n) - 1
 
 	for m > 0 && n > 0 {
 		num1, num2 := nums1[m-1], nums2[n-1]
 
 		if num1 > num2 {
-			nums1[pivot] = num1
+			nums1[writeIndex] = num1
 			m -= 1
 		} else {
-			nums1[pivot] = num2
+			nums1[writeIndex] = num2
 			n -= 1
 		}
-		pivot -= 1
+		writeIndex -= 1
 	}
 
 	for n > 0 {
-		nums1[pivot] = nums2[n-1]
-		pivot, n = pivot-1, n-1
+		nums1[writeIndex] = nums2[n-1]
+		writeIndex, n = writeIndex-1, n-1
 	}
 
 }
