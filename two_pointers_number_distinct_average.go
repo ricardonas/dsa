41f6e0d2eee2e491eb58741cf2e7bb0c72939d39
Ricardo Nascimento <43101705+ricardonas@users.noreply.github.com>
2025-01-17 15:58:03 -0300

    Create two_pointers_number_distinct_average.go

---
+++ two_pointers_number_distinct_average.go
@@ -0,0 +1,20 @@
+func distinctAverages(nums []int) int {
+    sort.Ints(nums)
+	l, r := 0, len(nums)-1
+
+	hasher := make(map[float64]*struct{})
+
+	for l < r {
+
+		avg := float64(nums[l] + nums[r]) / float64(2)
+
+		if _, ok := hasher[avg]; !ok {
+			hasher[avg] = nil
+		}
+		
+		l++
+		r--
+	}
+
+	return len(hasher)
+}
